74562a7e4175de3ebe15b58971a8f2bdfc0c86ea
shamitaaa <shamita.deshmukh@kellton.com>
2022-12-17 23:35:58 +0530

    added types and store

---
+++ src/store/types.ts
@@ -0,0 +1,47 @@
+export const GET_WEATHER = 'GET_WEATHER';
+export const SET_LOADING = 'SET_LOADING';
+export const SET_ERROR = 'SET_ERROR';
+
+export interface Weather {
+    description: string;
+    icon: string;
+    id: number;
+    main: string;
+}
+
+export interface WeatherData {
+    weather: any;
+    base: string;
+    cod: number;
+    coord: {
+        lon: number;
+        lat: number;
+    };
+    dt: number;
+    id: number;
+    main: {
+        temp: number;
+        temp_max: number;
+        temp_min: number;
+    };
+    name: string;
+    sys: {
+        country: string;
+        id: number;
+        type: number;
+    };
+}
+
+
+export interface WeatherState {
+    data: WeatherData | null;
+    loading: boolean;
+    error: string;
+}
+
+interface GetWeatherAction {
+    type: typeof GET_WEATHER;
+    payload: WeatherData
+}
+
+export type WeatherAction = GetWeatherAction ;
