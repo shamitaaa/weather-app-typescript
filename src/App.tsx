8b780a73f22d10a4545c182d5d04abe42b66be91
shamitaaa <shamita.deshmukh@kellton.com>
2022-12-18 00:02:10 +0530

    added reducer

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { FC } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import './App.css';
+import MainPage from './components/MainPage';
 
-function App() {
+const App: FC = () => {
+  const dispatch = useDispatch();
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="has-text-centered">
+      
+      
     </div>
   );
 }
--- src/store/types.ts
+++ src/store/types.ts
@@ -1,6 +1,4 @@
 export const GET_WEATHER = 'GET_WEATHER';
-export const SET_LOADING = 'SET_LOADING';
-export const SET_ERROR = 'SET_ERROR';
 
 export interface Weather {
     description: string;
